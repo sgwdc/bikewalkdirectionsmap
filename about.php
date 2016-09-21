f0f473a866da956b65490f1ece2dbf394089aa35
Steven Greenwaters <swaters@livingstreets.com>
2016-09-21 13:19:57 -0400

    Update About page

--- about.php
+++ about.php
@@ -1,4 +1,5 @@
 <?php
+/* From old website
 $status = "RaleighBPAC.org: Interactive Map: ABOUT PAGE";
 $CookieDomain = "raleighbpac.org";
 $CookieName = "RaleighBPACVisitorID";
@@ -7,30 +8,27 @@ $RelativeToRoot = "../../";
 //$RelativeToRoot = "../../../../Main/Web Media/";
 include $RelativeToRoot . "site_variables.php";
 include $RelativeToRoot . 'header.php';
+*/
 /* For running locally: (also comment out the two includes above)
 function isIgnoredIP() {
 	return true;
 }
 */
 ?>
-
-
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>About | Raleigh BPAC Planning Map</title>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
 </head>
-
 <body>
 <h3>Raleigh BPAC Planning Map - About</h3>
 
-<p>This interactive map shows current, future, and long-term planned bicycle facilities in Raleigh, North Carolina. "Future" means within about the next year.</p>
-<p>Facility types included are bike lanes, sharrows (shared lane markings), wide outside lanes, and miscellaneous.</p>
+<p>This interactive map shows current, future, and long-term planned bicycle facilities in Raleigh, North Carolina. This was designed to help the City of Raleigh's Bicycle &amp; Pedestrian Advisory Commission visualize current and proposed facilities to help prioritize funding.Facility types included are bike lanes, sharrows (shared lane markings), wide outside lanes, and miscellaneous.</p>
+<p>Some map layers were converted from ArcGIS shapefiles, and others were hand drawn.</p>
 <p>The background is the Raleigh City Council districts on top of the Google Bicycle Layer, which shows greenways and other current bike facilities. The Google Bicycle layer contains mostly greenways, and some on-street facilities.</p>
-<p>Please <a href="http://livingstreets.com/contact.php" target="_blank">contact Steven Waters</a> with any questions.</p>
+<p>Please <a href="mailto:swaters@livingstreets.com" target="_blank">contact Steven Greenwaters</a> with any questions.</p>
 <p><a href="./">Return to interactive map</a>.</p>
 <p>&nbsp;</p>
 </body>
