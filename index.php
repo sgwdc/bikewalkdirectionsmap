57126adfc6ea49ebfc56b53a59a929a44a54a91e
Steven Greenwaters <swaters@livingstreets.com>
2016-09-22 03:47:54 -0400

    Upgrade jQuery v2.2.4 to v3.1.0, and the jQuery Migrate plugin from v1.4.1 to v3.0.0.

--- index.php
+++ index.php
@@ -74,9 +74,9 @@ function isIgnoredIP() {
 	<?php //}; ?>
 
 	<!-- Load jQuery (Currently only using for Hide Menu) -->
-    <script src="js/jquery-2.2.4.min.js"></script>
+    <script src="js/jquery-3.1.0.min.js"></script>
     <!-- jQuery Migrate plugin to ensure compatibility with jQuery v2 -->
-    <script src="js/jquery-migrate-1.4.1.js"></script>
+    <script src="js/jquery-migrate-3.0.0.js"></script>
     <!-- Not currently using since autocomplete is disabled
     <script src="js/jquery-ui-1.8.2.custom.min.js"></script>
     -->
