ece9a2828c4a245cde524e073f6cecb2bdd2b5c0
Steven Greenwaters <swaters@livingstreets.com>
2016-12-01 19:18:25 -0500

    About Page: Update HTML and page title

--- about.php
+++ about.php
@@ -18,12 +18,12 @@ function isIgnoredIP() {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Interactive Map: Bike/Walk/Transit Trip Directions</title>
+    <title>About | Bike/Walk/Transit Trip Planning Directions Interactive Map</title>
     <meta name="viewport" content="initial-scale=1.0">
     <meta charset="utf-8">
 </head>
 <body>
-<h3>Interactive Map: Bike/Walk/Transit Trip Directions</h3>
+<h3>Bike/Walk/Transit Trip Directions Planning Interactive Map</h3>
 
 <p>This web application uses the Google Maps API and jQuery to provide users with multiple routes for bicycling, walking or riding transit between two locations.</p>
 <p>A location can be geocoded on the map by typing in an address, city and state. Alternatively, an address, city and state can be reverse geocoded by simply clicking anywhere on the map. Users can also display Google's Bicycle, Transit and Traffic layers in the background.</p>
