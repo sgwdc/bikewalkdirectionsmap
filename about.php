f42e4e46c1ccfaf2eed0cc759c7b4653e4e7dfdb
Steven Greenwaters <swaters@livingstreets.com>
2016-12-08 23:51:40 -0500

    About page: Update link from my email to the Living Streets contact page

--- about.php
+++ about.php
@@ -20,7 +20,7 @@
 
 	<p>This web application uses the Google Maps API and jQuery to provide users with multiple routes for bicycling, walking or riding transit between two locations.</p>
 	<p>A location can be geocoded on the map by typing in an address, city and state. Alternatively, an address, city and state can be reverse geocoded by simply clicking anywhere on the map. Users can also display Google's Bicycle, Transit and Traffic layers in the background.</p>
-	<p>Please <a href="mailto:swaters@livingstreets.com" target="_blank">contact Steven Greenwaters</a> with any questions.</p>
+	<p>Please <a href="/contact/" target="_blank">contact Steven Greenwaters</a> with any questions.</p>
 	<p><a href="./">Return to interactive map</a>.</p>
 	<p>&nbsp;</p>
 </body>
